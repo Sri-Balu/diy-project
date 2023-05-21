2b5eb0e8f5b16820ef56042665b31d72eb4a1835
balaji <sribalaji1420@gmail.com>
2023-05-21 15:00:15 +0530

    added login page

--- src/app/login/otppage/otppage.component.ts
+++ src/app/login/otppage/otppage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-otppage',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./otppage.component.css']
 })
 export class OTPpageComponent {
+  showRetryMessage: boolean;
+  retryCountdown: number;
 
+  constructor(private router: Router) {
+    this.showRetryMessage = false;
+    this.retryCountdown = 5; // Set the initial countdown time in seconds
+  }
+
+  handleRetryButtonClick(): void {
+    // Trigger the OTP resend functionality
+    // Start the countdown
+    this.startCountdown();
+  }
+
+  startCountdown(): void {
+    this.showRetryMessage = true;
+
+    const countdownInterval = setInterval(() => {
+      this.retryCountdown--;
+      if (this.retryCountdown <= 0) {
+        clearInterval(countdownInterval);
+        this.showRetryMessage = false;
+      }
+    }, 1000); // Update the countdown every second (1000ms)
+  }
 }
