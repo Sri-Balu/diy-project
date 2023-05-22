5f5719148e8291b8b62265793a2c3afbba877c66
balaji <sribalaji1420@gmail.com>
2023-05-22 16:52:59 +0530

    Added changes related to checkbox

--- src/app/login/loginpage/loginpage.component.ts
+++ src/app/login/loginpage/loginpage.component.ts
@@ -10,19 +10,37 @@ import Swal from 'sweetalert2';
   styleUrls: ['./loginpage.component.css']
 })
 export class LoginpageComponent {
-  isCheckboxChecked: boolean= true;
-  constructor(public router: Router, public localstorageService: LocalstorageService){
-    localStorage.setItem('PhoneNumber', '+911234567890');
-    if(localStorage.getItem('loggedin') == "true" && localstorageService.isloggedin == true) {
+  localStorage: any;
+  isChecked: boolean = false;
+  constructor(public router: Router, public localstorageService: LocalstorageService) {
+    if (localStorage.getItem('loggedin') == "true" && localstorageService.isloggedin == true) {
       this.router.navigateByUrl("/masterhome");
+    } else{
+      localStorage.setItem('isCheckboxChecked', 'false');
+      localStorage.setItem('PhoneNumber', '+911234567890');
     }
 
 
+
+
   }
-  navotp(){
+
+  checkBox(){
+      this.isChecked = !this.isChecked;
+  
+    // if (this.isChecked) {
+    //   this.localStorage.isCheckboxChecked = "true";
+    //   this.isChecked = true;
+    // } else {
+    //   this.localStorage.isCheckboxChecked = "false";
+    //   this.isChecked = false;
+    // }
+  }
+  navotp() {
     const enteredPhoneNumber = (<HTMLInputElement>document.getElementById('phoneNumber')).value;
     const storedPhoneNumber = localStorage.getItem('PhoneNumber');
-    if (enteredPhoneNumber === storedPhoneNumber && this.isCheckboxChecked==true) {
+  
+    if (enteredPhoneNumber === storedPhoneNumber &&  this.isChecked) {
       this.router.navigate(['/otp']);
     } else {
       Swal.fire('Invalid Phone Number', 'Please check your phone number', 'error');
