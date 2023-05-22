69e6d1abc4cb1401e6091787ebb63959cb21c056
balaji <sribalaji1420@gmail.com>
2023-05-22 13:32:23 +0530

    Changes to menu

--- src/app/home/menu/menu.component.ts
+++ src/app/home/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalstorageService } from 'src/app/local/localstorage.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  constructor(public router: Router, public localstorageService: LocalstorageService){}
+
+  navcrud(){
+    this.router.navigateByUrl('/crud')
+  }
+  navdashboard(){
+    this.router.navigateByUrl('dashboard')
+  }
+  navwallet(){
+    this.router.navigateByUrl('wallet')
+  }
+  navnotifications(){
+    this.router.navigateByUrl('notifications')
+  }
 
 }
