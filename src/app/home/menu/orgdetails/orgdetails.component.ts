a991191a62a933a8dcbd2ec09b17bf861b457d7e
anvesh-07 <settipallianveshreddy2002@gmail.com>
2023-05-22 15:08:56 +0530

    sweet alert to organigations

--- src/app/home/menu/orgdetails/orgdetails.component.ts
+++ src/app/home/menu/orgdetails/orgdetails.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-orgdetails',
   templateUrl: './orgdetails.component.html',
   styleUrls: ['./orgdetails.component.css']
 })
 export class OrgdetailsComponent {
+  showUnderConstructionAlert(name: string): void {
+    Swal.fire(`${name} Under Construction`, '', 'info');
+  }
 
 }
